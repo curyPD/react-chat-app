362d0dc141bb98d89ce087970bbefdef2d44b246
curyPD <romandruzhinin2003@gmail.com>
2022-10-03 11:05:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { IoLogoFirebase, IoLogoReact } from "react-icons/io5";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="container">
+            <header className="header">
+                <div className="logo">
+                    <div className="logo__box logo__box--react">
+                        <IoLogoReact className="logo__icon logo__icon--react" />
+                    </div>
+                    <div className="logo__box logo__box--firebase">
+                        <IoLogoFirebase className="logo__icon logo__icon--firebase" />
+                    </div>
+                </div>
+                <button className="btn btn--login">Log In</button>
+            </header>
+
+            <main className="main">
+                <section className="messages"></section>
+                <form className="form">
+                    <input type="text" className="input" />
+                    <button className="btn btn--submit">Send</button>
+                </form>
+            </main>
+        </div>
+    );
 }
-
-export default App;
