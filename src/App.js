4be2bc9564ebcbdd170370b2a1272116887d9682
curyPD <romandruzhinin2003@gmail.com>
2022-10-04 18:17:26 +0300

    Finish mobile design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from "react";
-import { IoLogoFirebase, IoLogoReact } from "react-icons/io5";
+import {
+    IoLogoFirebase,
+    IoLogoReact,
+    IoChatbubbleEllipses,
+} from "react-icons/io5";
 
 export default function App() {
     return (
         <div className="container">
             <header className="header">
                 <div className="logo">
-                    <div className="logo__box logo__box--react">
+                    {/* <div className="logo__box logo__box--react">
                         <IoLogoReact className="logo__icon logo__icon--react" />
                     </div>
                     <div className="logo__box logo__box--firebase">
                         <IoLogoFirebase className="logo__icon logo__icon--firebase" />
-                    </div>
+                    </div> */}
+
+                    <IoChatbubbleEllipses className="logo__icon" />
+                    <h1 className="logo__text">ChatApp</h1>
                 </div>
                 <button className="btn btn--login">Log In</button>
             </header>
 
             <main className="main">
-                <section className="messages"></section>
-                <form className="form">
-                    <input type="text" className="input" />
-                    <button className="btn btn--submit">Send</button>
-                </form>
+                <section className="messages-section"></section>
+                <section className="form-section">
+                    <form className="form">
+                        <input
+                            type="text"
+                            className="input"
+                            placeholder="Hi everyone, I'm..."
+                        />
+                        <button className="btn btn--submit">Send</button>
+                    </form>
+                </section>
             </main>
         </div>
     );
