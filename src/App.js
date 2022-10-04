74660cd6c98c93b2954274e9847680ea8ab28610
curyPD <romandruzhinin2003@gmail.com>
2022-10-04 19:34:21 +0300

    Set up Google Auth

--- src/App.js
+++ src/App.js
@@ -1,40 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { IoChatbubbleEllipses } from "react-icons/io5";
+
+import { app } from "./firebase";
 import {
-    IoLogoFirebase,
-    IoLogoReact,
-    IoChatbubbleEllipses,
-} from "react-icons/io5";
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut,
+} from "firebase/auth";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export default function App() {
+    const [curUser, setCurUser] = useState(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            console.log(user);
+            setCurUser(user);
+        });
+    }, []);
+
+    async function logIn() {
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(errorCode, errorMessage, email, credential);
+        }
+    }
+
     return (
         <div className="container">
             <header className="header">
                 <div className="logo">
-                    {/* <div className="logo__box logo__box--react">
-                        <IoLogoReact className="logo__icon logo__icon--react" />
-                    </div>
-                    <div className="logo__box logo__box--firebase">
-                        <IoLogoFirebase className="logo__icon logo__icon--firebase" />
-                    </div> */}
-
                     <IoChatbubbleEllipses className="logo__icon" />
                     <h1 className="logo__text">ChatApp</h1>
                 </div>
-                <button className="btn btn--login">Log In</button>
+                <button
+                    className="btn btn--login"
+                    onClick={() => {
+                        if (curUser) {
+                            signOut(auth);
+                            return;
+                        } else if (!curUser) {
+                            logIn();
+                            return;
+                        }
+                    }}
+                >
+                    {curUser ? "Sign Out" : "Log In"}
+                </button>
             </header>
 
             <main className="main">
                 <section className="messages-section"></section>
-                <section className="form-section">
-                    <form className="form">
-                        <input
-                            type="text"
-                            className="input"
-                            placeholder="Hi everyone, I'm..."
-                        />
-                        <button className="btn btn--submit">Send</button>
-                    </form>
-                </section>
+                {curUser && (
+                    <section className="form-section">
+                        <form className="form">
+                            <input
+                                type="text"
+                                className="input"
+                                placeholder="Hi everyone, I'm..."
+                            />
+                            <button className="btn btn--submit">Send</button>
+                        </form>
+                    </section>
+                )}
             </main>
         </div>
     );
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+};
+
+export const app = initializeApp(firebaseConfig);
