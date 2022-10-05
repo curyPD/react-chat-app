6290aee305b8d61de50d13714b41d7c0e07753ec
curyPD <romandruzhinin2003@gmail.com>
2022-10-05 18:31:28 +0300

    Write to database

--- src/App.js
+++ src/App.js
@@ -9,23 +9,84 @@ import {
     onAuthStateChanged,
     signOut,
 } from "firebase/auth";
+import { getDatabase, ref, set, push, onValue, get } from "firebase/database";
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const database = getDatabase();
 
 export default function App() {
     const [curUser, setCurUser] = useState(null);
+    // const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             console.log(user);
             setCurUser(user);
         });
+        /*const dbRef = ref(database, "messages");
+        onValue(dbRef, (snapshot) => {
+            // const arr = [];
+            snapshot.forEach(async (childSnapshot) => {
+                const childKey = childSnapshot.key;
+                const { sender, message } = childSnapshot.val();
+                let senderName, senderPhotoURL;
+                const userSnapshot = await get(
+                    ref(database, `users/${sender}`)
+                );
+                if (userSnapshot.exists()) {
+                    const userData = userSnapshot.val();
+                    senderName = userData.userName;
+                    senderPhotoURL = userData.photoURL;
+                } else {
+                    senderName = "Unknown user";
+                }
+                console.log(message, senderName, senderPhotoURL, childKey);
+                // arr.push({ message, senderName, senderPhotoURL, childKey });
+                setMessages((prevMessages) => [
+                    ...prevMessages,
+                    { message, senderName, senderPhotoURL, childKey },
+                ]);
+            });
+            // console.log(arr);
+            // setMessages(arr);
+        });*/
     }, []);
+
+    function handleChange(e) {
+        setInput(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const message = input;
+        const sender = curUser.uid;
+        setInput("");
+        writeMessageData(message, sender);
+    }
+
+    function writeMessageData(message, sender) {
+        const messageListRef = ref(database, "messages");
+        const newMessageRef = push(messageListRef);
+        set(newMessageRef, {
+            message,
+            sender,
+        });
+    }
+
+    function writeUserData(uid, userName, photoURL) {
+        set(ref(database, `users/${uid}`), {
+            userName,
+            photoURL,
+        });
+    }
 
     async function logIn() {
         try {
-            await signInWithPopup(auth, provider);
+            const result = await signInWithPopup(auth, provider);
+            const { displayName, photoURL, uid } = result.user;
+            writeUserData(uid, displayName, photoURL);
         } catch (error) {
             const errorCode = error.code;
             const errorMessage = error.message;
@@ -62,11 +123,14 @@ export default function App() {
                 <section className="messages-section"></section>
                 {curUser && (
                     <section className="form-section">
-                        <form className="form">
+                        <form className="form" onSubmit={handleSubmit}>
                             <input
                                 type="text"
                                 className="input"
                                 placeholder="Hi everyone, I'm..."
+                                name="input"
+                                value={input}
+                                onChange={handleChange}
                             />
                             <button className="btn btn--submit">Send</button>
                         </form>
